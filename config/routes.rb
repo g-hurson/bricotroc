9a76a5be47d56a597e048189753fa119bdf980a8
Theo Velon <theovelon@hotmail.fr>
2024-05-28 15:42:38 +0200

    changed main route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'bookings/show'
   get 'bookings/index'
   devise_for :users
-  root to: "pages#home"
+  root to: "tools#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
