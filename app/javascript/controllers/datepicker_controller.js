2d77ee8e5f830148bd78efacd12cfce09e600244
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-05-29 15:48:11 +0200

    refactoring Tool show

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,6 +4,11 @@ import flatpickr from "flatpickr"
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, {
+      defaultDate: "today", // Set the default date (optional)
+      minDate: "today", // Set the minimum date (mindate)
+      maxDate: "2024-12-31", // Set the maximum date (optional)
+      // Other options can be added here
+    })
   }
 }
