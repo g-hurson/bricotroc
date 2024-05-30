545cb98013bfa176a3980c084c1861784d9aebac
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-05-30 15:46:40 +0200

    refactor show version 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,46 @@ User.create(email: "geraldine@gmail.com", password: "1111")
 User.create(email: "theo@gmail.com", password: "2222")
 User.create(email: "joaquin@gmail.com", password: "3333")
 
+# Photo upload to Cloudinary
+
+
+
 #Tools for development
 
-Tool.new(name: "Perceuse B&D", description: "Perceuse Black & Decker sans fil", category: Tool::CATEGORIES[1], user: User.first)
-Tool.create(name: "Ponceuse multifunction", description: "Ponceuse delta multifonction 130W", category: Tool::CATEGORIES[0], user: User.first)
-Tool.create(name: "Scie circulaire", description: "Scie circulaire plongeante MAKITA SP6000J1 - 1300W - 165MM", category: Tool::CATEGORIES[3], user: User.last)
-Tool.create(name: "Visseuse à choc", description: "Lot outils MAKITA LCT204J (Perceuse Visseuse DF330D - Visseuse à Choc TD090D", category: Tool::CATEGORIES[2], user: User.last)
-Tool.create(name: "Kit de presse", description: "Ensemble D'Outils De Roulement De Vélo Outil D'Installation D'Outils De Montage De Moyeu", category: Tool::CATEGORIES[5], user: User.last)
+# tool = Tool.new(name: "Perceuse B&D", description: "Perceuse Black & Decker sans fil", category: Tool::CATEGORIES[1], user: User.first)
+# photos_perceuse = ["perceuse1.jpg", "perceuse2.jpg", "perceuse3.jpg"]
+# photos_perceuse.each do |photo|
+#  tool.photos.attach(io: File.open(Rails.root.join("app/assets/images/#{photos_perceuse.shift}")), filename: photo, content_type: "image/jpg")
+# end
+# tool.save
+
+# tool = Tool.new(name: "Ponceuse multifunction", description: "Ponceuse delta multifonction 130W", category: Tool::CATEGORIES[0], user: User.first)
+# photos_ponceuse = ["perceuse1.jpg", "perceuse2.jpg", "perceuse3.jpg"]
+# photos_ponceuse.each do |photo|
+#   tool.photos.attach(io: File.open(Rails.root.join("app/assets/images/#{photos_ponceuse.shift}")), filename: photo, content_type: "image/jpg")
+# end
+# tool.save
+
+tool = Tool.new(name: "Scie à bois", description: "Scie Jetcut™ STANLEY, coupe grossière, L 380 mm", category: Tool::CATEGORIES[3], user: User.last)
+photos_scie = ["scie1.jpeg", "scie2.jpeg", "scie3.jpeg"]
+photos_scie.each do |photo|
+  tool.photos.attach(io: File.open(Rails.root.join("app/assets/images/#{photos_scie.shift}")), filename: photo, content_type: "image/jpeg")
+end
+tool.save
+
+# tool = Tool.new(name: "Visseuse à choc", description: "Lot outils MAKITA LCT204J (Perceuse Visseuse DF330D - Visseuse à Choc TD090D", category: Tool::CATEGORIES[2], user: User.last)
+# photos_visseuse = ["perceuse1.jpg", "perceuse2.jpg", "perceuse3.jpg"]
+# photos_visseuse.each do |photo|
+#   tool.photos.attach(io: File.open(Rails.root.join("app/assets/images/#{photos_visseuse.shift}")), filename: photo, content_type: "image/jpg")
+# end
+# tool.save
+
+# tool = Tool.new(name: "Kit de presse", description: "Ensemble D'Outils De Roulement De Vélo Outil D'Installation D'Outils De Montage De Moyeu", category: Tool::CATEGORIES[5], user: User.last)
+# photos_kit = ["perceuse1.jpg", "perceuse2.jpg", "perceuse3.jpg"]
+# photos_kit.each do |photo|
+#   tool.photos.attach(io: File.open(Rails.root.join("app/assets/images/#{photos_kit.shift}")), filename: photo, content_type: "image/jpg")
+# end
+# tool.save
 
 # Bookings for development
 
